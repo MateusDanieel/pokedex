aa67353105e7a5fe743cf268a5703adf13393a9b
Mateus Silva <mateussilva@MacBook-Air-de-Mateus-2.local>
2026-09-02 23:11:16 -0300

    feat: add pokemon list render without stylesheet

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,57 @@
-import { getPokemonList, getPokemonDetails } from './api.js';
+import {
+  getPokemonList,
+  getPokemonDetails
+} from './api.js';
+
+const pokemonListElement = document.querySelector('.sec-pokemon-list__content');
 
 async function init() {
   try {
-    const { results } = await getPokemonList();
+    const { results } = await getPokemonList(20);
 
     const pokemonList = await Promise.all(
       results.map((pokemon) => getPokemonDetails(pokemon.url))
     );
 
-    console.log(pokemonList);
+    renderPokemonList(pokemonList);
   } catch (error) {
     console.error(error);
   }
+}
+
+function renderPokemonList(pokemonList) {
+  pokemonListElement.innerHTML = pokemonList
+    .map((pokemon) => createPokemonCard(pokemon))
+    .join('');
+}
+
+function createPokemonCard(pokemon) {
+  const types = pokemon.types
+    .map(({ type }) => `<span class="sec-pokemon-list__content__card__type">${type.name}</span>`)
+    .join('');
+
+  return `
+    <article class="sec-pokemon-list__content__card">
+      <span class="sec-pokemon-list__content__card__number">
+        #${String(pokemon.id).padStart(3, '0')}
+      </span>
+
+      <img
+        class="sec-pokemon-list__content__card__image"
+        src="${pokemon.sprites.other['official-artwork'].front_default}"
+        alt="${pokemon.name}"
+        loading="lazy"
+      >
+
+      <h2 class="sec-pokemon-list__content__card__name">
+        ${pokemon.name}
+      </h2>
+
+      <div class="sec-pokemon-list__content__card__types">
+        ${types}
+      </div>
+    </article>
+  `;
 }
 
 init();
